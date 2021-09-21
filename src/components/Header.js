9e0e9ee45a682a868c0369dd704db716f9c4f6a8
noel-srocha <manoelrocha.neto.93@hotmail.com>
2021-09-21 17:07:09 -0300

    Challenge 11 and 12 completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { useHistory } from 'react-router-dom';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 
 function Header({ value }) {
   const { pageName, setIcon } = value;
+  const history = useHistory();
+  const [showInput, setShowInput] = useState(false);
+
+  function isVerified() {
+    if (showInput === false) {
+      return setShowInput(true);
+    }
+    return setShowInput(false);
+  }
+
+  function hiddenInput() {
+    if (showInput) {
+      return (
+        <input type="text" data-testid="search-input" />
+      );
+    }
+  }
 
   function handleButton() {
     if (setIcon) {
       return (
-        <button
-          type="button"
-        >
-          <img data-testid="search-top-btn" src={ searchIcon } alt="buscar" />
-        </button>
+        <>
+          <button
+            type="button"
+            onClick={ isVerified }
+          >
+            <img data-testid="search-top-btn" src={ searchIcon } alt="buscar" />
+          </button>
+          { hiddenInput() }
+        </>
       );
     }
   }
@@ -22,6 +44,7 @@ function Header({ value }) {
     <header>
       <button
         type="button"
+        onClick={ () => history.push('/perfil') }
       >
         <img
           data-testid="profile-top-btn"
