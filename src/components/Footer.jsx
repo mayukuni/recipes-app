0c777fe5e579902057d143638b9510f66f73e456
Mayumi <cinthya.m.kuniyoshi@gmail.com>
2021-09-21 18:32:02 -0300

    requirement 19 to 24

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,8 @@ import exploreIcon from '../images/exploreIcon.svg';
 import mealIcon from '../images/mealIcon.svg';
 
 function Footer() {
-  const footerStyle = { marginTop: '100vh' };
+  const footerStyle = { position: 'fixed',
+    bottom: '0px' };
   return (
     <footer data-testid="footer" style={ footerStyle }>
       <Link to="/bebidas" data-testid="drinks-bottom-btn">
