54fb869e06e2f576687861c020d044506d9db319
noel-srocha <manoelrocha.neto.93@hotmail.com>
2021-09-21 17:46:38 -0300

    Connect Login and Header requirements

--- src/context/RecipeProvider.js
+++ src/context/RecipeProvider.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import RecipesContext from './RecipeContext';
 
 export default function RecipesProvider({ children }) {
+  const [email, setEmail] = useState('');
+
+  const context = {
+    email,
+    setEmail,
+  };
+
   return (
-    <RecipesContext.Provider>
+    <RecipesContext.Provider value={ context }>
       { children }
     </RecipesContext.Provider>
   );
