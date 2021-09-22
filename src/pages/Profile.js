612d4b88acd0abc03b97280e6bc6619b5f4e8c2c
Mayumi <cinthya.m.kuniyoshi@gmail.com>
2021-09-22 16:01:38 -0300

    requirement 82, 84, 85, 86

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
-export default function Profile() {
+export default function Profile({ history }) {
   const pageTitle = {
     pageName: 'Perfil',
     setIcon: false,
@@ -10,8 +11,38 @@ export default function Profile() {
   return (
     <div>
       <Header value={ pageTitle } />
-      FOODS
+      Perfil
+      <button
+        type="button"
+        data-testid="profile-done-btn"
+        onClick={ () => history.push('/receitas-favoritas') }
+      >
+        Receitas Feitas
+      </button>
+
+      <button
+        type="button"
+        data-testid="profile-favorite-btn"
+        onClick={ () => history.push('/receitas-feitas') }
+      >
+        Receitas Favoritas
+      </button>
+
+      {/* terminar o onClick dessa */}
+      <button
+        type="button"
+        data-testid="profile-logout-btn"
+        onClick={ () => history.push('/') }
+      >
+        Sair
+      </button>
       <Footer />
     </div>
   );
 }
+
+Profile.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
