e14d0e2b91e1e8d2ffbd3fec240d2727f7f11e1f
marcus <marcus.rodrigues10@hotmail.com>
2021-09-22 17:43:43 -0300

    solve problems

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,56 +1,67 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import RecipeContext from '../context/RecipeContext';
 
 export default function Profile({ history }) {
+  // const { email, setEmail } = useContext(RecipeContext);
   const pageTitle = {
     pageName: 'Perfil',
     setIcon: false,
   };
 
-  const email = JSON.parse(localStorage.getItem('user'));
+  const emailObj = JSON.parse(localStorage.getItem('user'));
+  // const { email } = emailObj;
+  // // setEmail(email);
 
   const outPage = () => {
     localStorage.clear();
     history.push('/');
   };
 
+  const test = () => {
+    if (emailObj === null) {
+      return <h4 data-testid="profile-email">Email</h4>;
+    } return <h4 data-testid="profile-email">{ emailObj.email }</h4>;
+  };
+
   return (
     <div>
       <Header value={ pageTitle } />
       <div>
-        <h4
-          data-testid="profile-email"
-        >
-          { email.email }
-        </h4>
+        { test() }
       </div>
       Perfil
-      <button
-        type="button"
-        data-testid="profile-done-btn"
-        onClick={ () => history.push('/receitas-feitas') }
-      >
-        Receitas Feitas
-      </button>
-
-      <button
-        type="button"
-        data-testid="profile-favorite-btn"
-        onClick={ () => history.push('/receitas-favoritas') }
-      >
-        Receitas Favoritas
-      </button>
+      <div>
+        <button
+          type="button"
+          data-testid="profile-done-btn"
+          onClick={ () => history.push('/receitas-feitas') }
+        >
+          Receitas Feitas
+        </button>
+      </div>
+      <div>
+        <button
+          type="button"
+          data-testid="profile-favorite-btn"
+          onClick={ () => history.push('/receitas-favoritas') }
+        >
+          Receitas Favoritas
+        </button>
+      </div>
 
       {/* terminar o onClick dessa */}
-      <button
-        type="button"
-        data-testid="profile-logout-btn"
-        onClick={ () => outPage() }
-      >
-        Sair
-      </button>
+      <div>
+        <button
+          type="button"
+          data-testid="profile-logout-btn"
+          onClick={ () => outPage() }
+        >
+          Sair
+        </button>
+      </div>
       <Footer />
     </div>
   );
