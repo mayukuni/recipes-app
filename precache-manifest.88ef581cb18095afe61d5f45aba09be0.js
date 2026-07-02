56e2493a9471907764207cf2325649c4d6043d1a
mayukuni <cinthya.m.kuniyoshi@gmail.com>
2026-07-02 00:05:19 -0300

    Updates

---
+++ precache-manifest.88ef581cb18095afe61d5f45aba09be0.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6c6e9732cc3059f6ba88471604cef04",
+    "url": "/recipes-app/index.html"
+  },
+  {
+    "revision": "803fea90c3630b5d8409",
+    "url": "/recipes-app/static/css/main.6be29d18.chunk.css"
+  },
+  {
+    "revision": "6fc7bdbcb672652661c2",
+    "url": "/recipes-app/static/js/2.5b64c2cb.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/recipes-app/static/js/2.5b64c2cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "803fea90c3630b5d8409",
+    "url": "/recipes-app/static/js/main.3fa1cc4a.chunk.js"
+  },
+  {
+    "revision": "25993f9b630f63b44851",
+    "url": "/recipes-app/static/js/runtime-main.1228032d.js"
+  },
+  {
+    "revision": "b89133460c9aef111d6e773396b5917d",
+    "url": "/recipes-app/static/media/blackHeartIcon.b8913346.svg"
+  },
+  {
+    "revision": "f87c47c37c0c0530fe7307af260caa25",
+    "url": "/recipes-app/static/media/drinkIcon.f87c47c3.svg"
+  },
+  {
+    "revision": "157945fffa619b919f7bb65fe11b7b43",
+    "url": "/recipes-app/static/media/exploreIcon.157945ff.svg"
+  },
+  {
+    "revision": "b27ba19ea033355b73926443af1756e9",
+    "url": "/recipes-app/static/media/mealIcon.b27ba19e.svg"
+  },
+  {
+    "revision": "74e487e504d5cf16d7832bef2fbacfea",
+    "url": "/recipes-app/static/media/profileIcon.74e487e5.svg"
+  },
+  {
+    "revision": "db81213596cb3beb56440bb39aa70c8c",
+    "url": "/recipes-app/static/media/searchIcon.db812135.svg"
+  },
+  {
+    "revision": "06215ed4cae23455c02abc804445ba74",
+    "url": "/recipes-app/static/media/shareIcon.06215ed4.svg"
+  },
+  {
+    "revision": "ea3b6ba893ba3908dd84f6170b75545e",
+    "url": "/recipes-app/static/media/whiteHeartIcon.ea3b6ba8.svg"
+  }
+]);
